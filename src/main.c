1404c0db4658b4818a82d06fa92c1cb18343480d
ABdeeglr <abdeeglr@icloud.com>
2024-04-09 09:40:55 +0800

    (update) renew int arrary constructor

--- src/main.c
+++ src/main.c
@@ -2,49 +2,35 @@
 #include "head.h"
 #include "f_sort/f_sort.h"
 #include <time.h>
-#include <locale.h>
-#define ARRAY_SIZE 100000
-int get_random_integer(int boundary);
 
+int* createIntArray(const int size, const int boundary); 
 
 int main(int argc, const char *argv[]) {
-    
-    int* ary = (int*) malloc(sizeof(int) * 20);
-    for (int i = 0; i < 20; i++) {
-        int temp = get_random_integer(1000);
-        printf("%d\t", temp);
-        ary[i] = temp;
-    }
-    puts("");
-    printArray(ary, 20);
-    selectSort(ary, 20);
-    printArray(ary, 20);
-    free(ary);
+    Pint sb = createIntArray(20, 100);
+    printArray(sb, 20);
+    free(sb);
 
+    
     return 0;
 }
 
-int get_random_integer(int boundary) {
-    int ret = rand() % boundary;
-    return ret;
-}
 
+int* createIntArray(const int size, const int boundary) {
+    Pint numbers = (Pint)malloc(sizeof(int) * size);
+    srand((unsigned) time(NULL));
 
-int* createIntArray() {
-    Pint numbers = (Pint)malloc(sizeof(int) * ARRAY_SIZE);
-
-    for (int i = 0; i < ARRAY_SIZE; i += 10)
+    for (int i = 0; i < size; i += 10)
     {
-        numbers[i + 0] = get_random_integer(500000);
-        numbers[i + 1] = get_random_integer(500000);
-        numbers[i + 2] = get_random_integer(500000);
-        numbers[i + 3] = get_random_integer(500000);
-        numbers[i + 4] = get_random_integer(500000);
-        numbers[i + 5] = get_random_integer(500000);
-        numbers[i + 6] = get_random_integer(500000);
-        numbers[i + 7] = get_random_integer(500000);
-        numbers[i + 8] = get_random_integer(500000);
-        numbers[i + 9] = get_random_integer(500000);
+        numbers[i + 0] = rand() % boundary;
+        numbers[i + 1] = rand() % boundary;
+        numbers[i + 2] = rand() % boundary;
+        numbers[i + 3] = rand() % boundary;
+        numbers[i + 4] = rand() % boundary;
+        numbers[i + 5] = rand() % boundary;
+        numbers[i + 6] = rand() % boundary;
+        numbers[i + 7] = rand() % boundary;
+        numbers[i + 8] = rand() % boundary;
+        numbers[i + 9] = rand() % boundary;
     }
     return numbers;
 }
